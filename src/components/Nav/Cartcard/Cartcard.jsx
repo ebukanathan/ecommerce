cff6bfaa684138956d7dfed1ac7a7b5b04cb8a5e
ebuka nathan <ebukanathan@gmail.com>
2024-05-21 23:23:42 +0100

    cartcard

--- src/components/Nav/Cartcard/Cartcard.jsx
+++ src/components/Nav/Cartcard/Cartcard.jsx
@@ -8,7 +8,7 @@ function Cartcard({ img, title, price, handleDelete }) {
           <img src={img} alt="" className={style.cartimg} />
           <h6 className={style.title}>{title}</h6>
         </div>
-        <input type="number" className={style.quantity} min={1} />
+        {/* <input type="number" className={style.quantity} min={1} /> */}
         <div className={style.subtotal}>{price}</div>
         <button className={style.delete} onClick={handleDelete}>
           Remove
